5f6284e65224951219f4c615518e0d5f9cddad78
Ipsit Sahoo (Student) <isahoo1@asurite.asu.edu>
2023-10-07 23:09:25 -0700

    Add back-end for item query

--- backend/controllers/ItemInventoryInfoController/getItemInventoryInfoController.js
+++ backend/controllers/ItemInventoryInfoController/getItemInventoryInfoController.js
@@ -1,9 +1,12 @@
 const ItemInventoryInfoModel = require('../../models/ItemInventoryInfoModel')
 
 const getItemInventoryInfoController = async (req, res)=>{
-
-    const result = await ItemInventoryInfoModel.find()
-
+    var result = [];
+    if(req.query.lowestCount) {
+        result = await ItemInventoryInfoModel.find().sort({ itemQuantity : 1}).limit(5);
+    } else {
+        result = await ItemInventoryInfoModel.find()
+    }
     res.send(result)
 }
 
--- front_end/src/Components/Donations/Donations.jsx
+++ front_end/src/Components/Donations/Donations.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import { Form, Button, Container, Row, Col, Table } from 'react-bootstrap';
 import './Donations.css'
@@ -7,6 +8,7 @@ function Donations() {
   const [itemQuantity, setItemQuantity] = useState('');
   const [expiryDate, setExpiryDate] = useState('');
   const [itemType, setItemType] = useState('');
+  const [dataItems, setDataItems] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -26,47 +28,26 @@ function Donations() {
     setItemType('');
   };
 
-  const dummyDonationItems = [
-    {
-      id: 1,
-      itemName: "Blankets",
-      itemQuantity: 20,
-      itemType: "Warmth",
-    },
-    {
-      id: 2,
-      itemName: "Canned Food",
-      itemQuantity: 50,
-      itemType: "Food",
-    },
-    {
-      id: 3,
-      itemName: "Toothbrushes",
-      itemQuantity: 100,
-      itemType: "Hygiene",
-    },
-    {
-      id: 4,
-      itemName: "Socks",
-      itemQuantity: 80,
-      itemType: "Clothing",
-    },
-    {
-      id: 5,
-      itemName: "Shampoo",
-      itemQuantity: 30,
-      itemType: "Hygiene",
-    }
-  ];
+  axios.get('http://localhost:8000/itemInventory/getItemInventoryInfo?lowestCount=5')
+      .then(res => {
+          // console.log(res.data);
+          setDataItems(res.data);
+
+      });
 
   return (
     <Container>
       <h4 className='flashy-highlight'>Join Us in Supporting Our Troops: Donate Essential Items Today!</h4>
 
       <p className='text-center'>
-      Join us in honoring our military heroes by contributing items that can enhance their daily lives while they protect our freedoms. Your donations of personal care items, snacks, and other necessities are greatly appreciated.
+      Our dedicated military members endure demanding conditions during their service. Your generosity in 
+      donating essential items, such as socks, hygiene products, and non-perishable snacks, can profoundly 
+      impact their well-being. Your support will aid us in providing these heroes with the crucial necessities they require.
       </p>
 
+      <blockquote className='blockquote'>
+        Below is a list of items that will greatly assist us in supporting our troops:
+      </blockquote>
       <Table striped bordered hover>
         <thead>
           <tr>
@@ -77,7 +58,7 @@ function Donations() {
           </tr>
         </thead>
         <tbody>
-          {dummyDonationItems.map((item, index) => (
+          {dataItems.map((item, index) => (
             <tr key={item.id}>
               <td>{index + 1}</td>
               <td>{item.itemName}</td>

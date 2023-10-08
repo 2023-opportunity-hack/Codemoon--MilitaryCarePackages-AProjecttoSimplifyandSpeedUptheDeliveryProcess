0de83a3d6391c93a9fea94cdc58cb84f8b81b0a3
Ipsit Sahoo (Student) <isahoo1@asurite.asu.edu>
2023-10-08 12:45:54 -0700

    Completed RBAC

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,13 +1,13 @@
 import { CustomerForm, CustomNavbar, Donations, InventoryView, LoginForm } from "./Components";
 import MilitaryCarePackageRequests from './Components/MilitaryCarePackageRequests/MilitaryCarePackageRequests'
 import Customs from './Components/Customs/Customs'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import './App.css';
 
 
 
 function App() {
-
+  
   return (
     <div className="App">
       <Router>
@@ -16,7 +16,7 @@ function App() {
             <Route path='/' element={<CustomerForm />}/>
             <Route path='/donations' element={< Donations />}/>
             <Route path="/inventoryView" element={ < InventoryView />} />
-            <Route path='/login' element={ <LoginForm /> } />
+            <Route exact path='/login' element={< LoginForm /> } />
             <Route path="/military-care-package-requests" element={<MilitaryCarePackageRequests />} />
             <Route path="/customs" element={<Customs />} />
         </Routes>
--- front_end/src/Components/CustomNavbar/CustomNavbar.jsx
+++ front_end/src/Components/CustomNavbar/CustomNavbar.jsx
@@ -1,26 +1,52 @@
 import React from 'react';
-import { BrowserRouter as Router, Link } from "react-router-dom";
-import { Navbar, Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Link, useNavigate } from "react-router-dom";
+import { Navbar, Container, Row, Col } from 'react-bootstrap';
 import "./CustomNavbar.css"
 
 
 const CustomNavbar = () => {
   const jwtToken = localStorage.getItem('token');
+  const role = localStorage.getItem('role');
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Clear local storage (remove the JWT or any other stored data)
+    localStorage.removeItem('token'); // Replace 'token' with the actual key you're using
+    localStorage.removeItem('role');
+
+    // Redirect to the home page
+    navigate('/');
+  };
 
   return (
     <Navbar className="custom-navbar" expand="lg">
       <Container>
         {/* Space for your icon */}
-        <Navbar.Brand href="/">
-          <img
-            src="bsma.webp" // Replace with the path to your icon image
-            alt="Icon"
-            width="100"
-            height="30"
-            className="d-inline-block align-top"
-          />
-        </Navbar.Brand>
-
+        <Row>
+          <Col>
+              {jwtToken && role === '0' ? (
+                  <div className="box admin-box">
+                    <span className="navbar-text text-white">Admin View</span>
+                  </div>
+                ) : (
+                  <div className="box admin-box">
+                    <span className="navbar-text text-white">General View</span>
+                  </div>
+                )}
+          </Col>
+          <Col>
+              <Navbar.Brand href="/">
+              <img
+                src="bsma.webp" // Replace with the path to your icon image
+                alt="Icon"
+                width="100"
+                height="30"
+                className="d-inline-block align-top"
+              />
+            </Navbar.Brand>
+          </Col>
+        </Row>
         {/* Navbar links can go here */}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -28,10 +54,10 @@ const CustomNavbar = () => {
             <li className="nav-item">
               <Link to="/" className='nav-link text-white'> Home </Link> 
             </li>
-            {jwtToken && ( <>
-              <li className="nav-item">
-                <Link to="/donations" className='nav-link text-white'> Donations </Link>
-              </li>
+            <li className="nav-item">
+              <Link to="/donations" className='nav-link text-white'> Donations </Link>
+            </li>
+            {jwtToken && role === '0' && ( <>
               <li className='nav-item'>
                 <Link to='/inventoryView' className='nav-link text-white'> Inventory </Link>
               </li>
@@ -43,12 +69,18 @@ const CustomNavbar = () => {
             <li className='nav-item'>
               <Link to='/login' className='nav-link text-white'> Login </Link>
             </li>}
-            <li className="nav-item">
-              <Link to="/donations" className='nav-link text-white'> Donations </Link>
-            </li>
             <li>
               <div id="google_translate_element"></div>
             </li>
+
+            {jwtToken && (
+              <>
+                <li className='nav-item'>
+                  <a onClick={handleLogout} className='nav-link text-white'>Logout</a>
+                </li>
+              </>
+            )}
+
             {/* Add more links as needed */}
           </ul>
         </Navbar.Collapse>

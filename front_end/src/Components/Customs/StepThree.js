eb60e3edc2d33a3fbffbe2305514c984c100f3e7
Hartik Suhagiya <hartiksuhagiya10@gmail.com>
2023-10-08 11:08:52 -0700

    added Shibbing label and customs page

---
+++ front_end/src/Components/Customs/StepThree.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const StepThree = () => {
+    return (
+        <div style={{ width: "50vw", margin: "1rem auto" }}>
+            <h2>Prohibited Items</h2>
+            <ul>
+                <li>Aerosols</li>
+                <li>Air Bags</li>
+                <li>Alcoholic Beverages</li>
+                <li>Ammunition</li>
+                <li>Cigarettes</li>
+                <li>Dry Ice</li>
+                <li>Explosives</li>
+                <li>Fresh Fruits and Vegetables</li>
+                <li>Gasoline</li>
+                <li>Marijuana (medical or otherwise)</li>
+                <li>Nail Polish</li>
+                <li>Perfumes (containing alcohol)</li>
+                <li>Poisons</li>
+            </ul>
+        </div>
+    )
+}
+
+export default StepThree
